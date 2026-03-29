294fcf42acbb3911ef9b51405fedd811ae642c06
triccina72 <triccina72@gmail.com>
2026-03-29 20:01:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const { google } = require('googleapis');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
